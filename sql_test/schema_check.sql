a029d907fd24a491b35546772d75e69b51cf18ee
李一玚 <henryly94@gmail.com>
2017-03-01 23:14:51 +0800

    [windows] first commit & add some sql schema check scrpit

---
+++ sql_test/schema_check.sql
@@ -0,0 +1,3 @@
+select PaperID,OriginalPaperTitle,ConferenceSeriesIDMappedToVenueName from Papers where PaperPublishYear = 2013;
+select ShortName,ConferenceSeriesID,ConferenceSeriesID from ConferenceSeries where ConferenceSeriesID = '42D7146F' or ConferenceSeriesID = '43510C43';
+
